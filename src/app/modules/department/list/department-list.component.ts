79e53ecb43857714c3c661fa9d8c0dd54a874521
黄大超 <dchuang@incardata.com.cn>
2016-12-12 21:59:42 +0800

    .

--- src/app/modules/department/list/department-list.component.ts
+++ src/app/modules/department/list/department-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'department-list',
+    styleUrls: ['./department-list.component.scss'],
     templateUrl: './department-list.component.html',
 })
 export class DepartmentListComponent {
